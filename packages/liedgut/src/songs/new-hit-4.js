64618a132fc331267cb977397afc2e342830e56b
Robin Weser <contact@weser.io>
2020-03-24 20:27:34 +0100

    add new-hit-4

---
+++ packages/liedgut/src/songs/new-hit-4.js
@@ -0,0 +1,26 @@
+export default {
+  "translation": [],
+  "beat": "4/4",
+  "tempo": 60,
+  "title": "new hit #4",
+  "info": "foo = bar",
+  "content": "Bar Baz. {D}\nFoo {F}",
+  "words": [
+    {
+      "nickname": "",
+      "group": "",
+      "organisation": "",
+      "name": "foo bar",
+      "year": "2020"
+    }
+  ],
+  "tune": [
+    {
+      "nickname": "",
+      "group": "",
+      "organisation": "",
+      "name": "foo bar",
+      "year": "2020"
+    }
+  ]
+}
