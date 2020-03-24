6d277eeb451da65b458b1bf0f16f0835ab2d4891
Robin Weser <contact@weser.io>
2020-03-24 20:37:03 +0100

    add new-hit-version-5

---
+++ packages/liedgut/src/songs/new-hit-version-5.js
@@ -0,0 +1,26 @@
+export default {
+  "translation": [],
+  "beat": "4/4",
+  "tempo": 60,
+  "title": "New hit version 5",
+  "info": "Cool shit",
+  "content": "New song.\n\n\nSome lines. {D}",
+  "words": [
+    {
+      "group": "",
+      "organisation": "",
+      "name": "foo",
+      "nickname": "bar",
+      "year": "20020"
+    }
+  ],
+  "tune": [
+    {
+      "group": "",
+      "organisation": "",
+      "name": "foo",
+      "nickname": "bar",
+      "year": "20020"
+    }
+  ]
+}
