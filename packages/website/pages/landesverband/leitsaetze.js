5df0757bbd552a245c4b36a7e7a6071d325e979a
Robin Weser <contact@weser.io>
2020-04-01 20:12:24 +0200

    simplify navigation, add landesleitung

--- packages/website/pages/landesverband/leitsaetze.js
+++ packages/website/pages/landesverband/leitsaetze.js
@@ -14,36 +14,10 @@ import {
 import Layout from '../../components/Layout'
 import Template from '../../components/Template'
 
-const subNav = {
-  '/landesleitung': 'Landesleitung',
-  '/staemme': 'Stämme',
-  '/leitsaetze': 'Leitsätze',
-  '/club-29': 'Club 29',
-  '/watato-kabisa': 'Watato Kabisa',
-  '/geschichte': 'Geschichte',
-}
-
-export default () => {
-  const router = useRouter()
-
-  return (
-    <Template>
-      <NavBar intent="secondary">
-        <Layout>
-          <Box direction={['column', , 'row']} paddingLeft={5}>
-            {Object.keys(subNav).map(path => (
-              <NavBarItem
-                href={'/landesverband' + path}
-                active={router.pathname.indexOf(path) !== -1}>
-                {subNav[path]}
-              </NavBarItem>
-            ))}
-          </Box>
-        </Layout>
-      </NavBar>
-      <Layout paddingTop={10} paddingBottom={10}>
-        Leitsätze
-      </Layout>
-    </Template>
-  )
-}
+export default () => (
+  <Template>
+    <Layout paddingTop={10} paddingBottom={10}>
+      Leitsätze
+    </Layout>
+  </Template>
+)
