6f3ce3166004b0eefb7bbaf3b75e4f1eb6262a07
Robin Weser <contact@weser.io>
2020-04-01 20:13:56 +0200

    remove old navs

--- packages/website/pages/digital/index.js
+++ packages/website/pages/digital/index.js
@@ -14,33 +14,10 @@ import {
 import Layout from '../../components/Layout'
 import Template from '../../components/Template'
 
-const subNav = {
-  '/downloads': 'Downloads',
-  '/cloud': 'Cloud',
-  '/apps': 'Apps',
-}
-
-export default () => {
-  const router = useRouter()
-
-  return (
-    <Template>
-      <NavBar intent="secondary">
-        <Layout>
-          <Box direction={['column', , 'row']} paddingLeft={5}>
-            {Object.keys(subNav).map(path => (
-              <NavBarItem
-                href={'/digital' + path}
-                active={router.pathname.indexOf(path) !== -1}>
-                {subNav[path]}
-              </NavBarItem>
-            ))}
-          </Box>
-        </Layout>
-      </NavBar>
-      <Layout paddingTop={10} paddingBottom={10}>
-        Digital
-      </Layout>
-    </Template>
-  )
-}
+export default () => (
+  <Template>
+    <Layout paddingTop={10} paddingBottom={10}>
+      Digital
+    </Layout>
+  </Template>
+)
