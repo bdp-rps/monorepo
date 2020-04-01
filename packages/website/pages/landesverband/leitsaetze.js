c8c4dc665ed5adce97c2cb1c8a1711ecef70e56c
Robin Weser <contact@weser.io>
2020-04-01 20:20:57 +0200

    remove nav from watato

--- packages/website/pages/landesverband/leitsaetze.js
+++ packages/website/pages/landesverband/leitsaetze.js
@@ -17,41 +17,55 @@ import Template from '../../components/Template'
 
 export default () => (
   <Template>
-        <Layout paddingTop={10} paddingBottom={10}>
-        <Box>
-          <Text>
-            <Text intent="category">Willkommen bei den Pfadfindern.</Text>
+    <Layout paddingTop={10} paddingBottom={10}>
+      <Box>
+        <Text>
+          <Text intent="category">Willkommen bei den Pfadfindern.</Text>
+          <br />
+          Wie sich Seeleute an den Sternen orientierten, so braucht auch eine
+          Gemeinschaft feste Richtwerte, die von dauerhafter Gültigkeit sind.
+          Wir, die Stämme im Landesverband Rheinland-Pfalz / Saar im Bund der
+          Pfadfinderinnen und Pfadfinder, haben uns die folgenden Leitsätze
+          gegeben, um die Freundschaft und Verbundenheit zwischen unseren Stämme
+          zu fördern. Sie halten fest, was das Wesen unseres Landesverbandes
+          ausmacht. Sie sind Ziel und Anspruch zugleich.
+          <br />
+        </Text>
+      </Box>
+    </Layout>
+    <Layout
+      grow={1}
+      paddingTop={5}
+      paddingBottom={10}
+      alignSelf="stretch"
+      extend={{ backgroundColor: 'rgb(235, 235, 235)' }}>
+      <Text intent="subtitle">Wir wollen...</Text>
+      <Box paddingTop={2} direction={['column', , , 'row']} space={4}>
+        <Box grow={10}>
+          <Tile
+            title="1. einen Beitrag zur Persönlichkeitsbildung junger Menschen leisten"
+            image="/images/bg.jpg"
+            highlight>
+            Unser Ziel ist es, junge Menschen in der Entwicklung zu
+            eigenverantwortlichen, kritischen und engagierten Persönlichkeiten
+            zu unterstützen.
             <br />
-            Wie sich Seeleute an den Sternen orientierten, so braucht auch eine Gemeinschaft feste Richtwerte, die von dauerhafter Gültigkeit sind.
-            Wir, die Stämme im Landesverband Rheinland-Pfalz / Saar im Bund der Pfadfinderinnen und Pfadfinder, haben uns die folgenden Leitsätze gegeben,
-            um die Freundschaft und Verbundenheit zwischen unseren Stämme zu fördern. Sie halten fest, was das Wesen unseres Landesverbandes ausmacht.
-            Sie sind Ziel und Anspruch zugleich.
+            Unsere Mitglieder sollen in der Lage sein, Verantwortung für sich
+            selbst und für andere zu übernehmen. Die Fähigkeiten und Werte,
             <br />
-          </Text></Box>
-      </Layout>
-      <Layout
-        grow={1}
-        paddingTop={5}
-        paddingBottom={10}
-        alignSelf="stretch"
-        extend={{ backgroundColor: 'rgb(235, 235, 235)' }}>
-        <Text intent="subtitle">Wir wollen...</Text>
-        <Box paddingTop={2} direction={['column', , , 'row']} space={4}>
-          <Box grow={10}>
-            <Tile title="1. einen Beitrag zur Persönlichkeitsbildung junger Menschen leisten" image="/images/bg.jpg" highlight>
-            Unser Ziel ist es, junge Menschen in der Entwicklung zu eigenverantwortlichen, kritischen und engagierten Persönlichkeiten zu unterstützen.
+            die sie im Stamm und Landesverband erwerben, sollen sie auch
+            außerhalb unserer Gemeinschaft positiv in die Gesellschaft
+            einbringen.
             <br />
-            Unsere Mitglieder sollen in der Lage sein, Verantwortung für sich selbst und für andere zu übernehmen. Die Fähigkeiten und Werte,
-            <br />
-            die sie im Stamm und Landesverband erwerben, sollen sie auch außerhalb unserer Gemeinschaft positiv in die Gesellschaft einbringen.
-            <br />
-            Um dieses Ziel zu erreichen, müssen junge Leute in unserem Landesverband ein stimmiges Umfeld finden, in dem ihre Persönlichkeit wachsen kann.
-            </Tile>
-            <Tile title="Aktueller Post" image="/images/bg.jpg" highlight>
-              Das ist der aktuelle Post, der wird gehighlighted.
-            </Tile>
-          </Box>
+            Um dieses Ziel zu erreichen, müssen junge Leute in unserem
+            Landesverband ein stimmiges Umfeld finden, in dem ihre
+            Persönlichkeit wachsen kann.
+          </Tile>
+          <Tile title="Aktueller Post" image="/images/bg.jpg" highlight>
+            Das ist der aktuelle Post, der wird gehighlighted.
+          </Tile>
         </Box>
-      </Layout>
+      </Box>
+    </Layout>
   </Template>
 )
