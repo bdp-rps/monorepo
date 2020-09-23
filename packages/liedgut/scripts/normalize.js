434081e4f04fb32c42d1fff122f581e14ee4a87e
Robin Weser <contact@weser.io>
2020-09-23 13:51:55 +0200

    format all normalized songs

--- packages/liedgut/scripts/normalize.js
+++ packages/liedgut/scripts/normalize.js
@@ -20,7 +20,7 @@ fs.readdir('./src/songs', (err, files) => {
       song.content = normalizeContent(song.content)
       song.translation = song.translation || []
 
-      fs.writeFile('./src/songs/' + f, JSON.stringify(song), err => {
+      fs.writeFile('./src/songs/' + f, JSON.stringify(song, null, 2), err => {
         if (err) {
           console.error(err)
         }
