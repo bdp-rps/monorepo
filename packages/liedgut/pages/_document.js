c4731ea680e31afd19434c1b88e1b99add82485d
Robin Weser <contact@weser.io>
2020-09-26 11:34:01 +0200

    add tracking

--- packages/liedgut/pages/_document.js
+++ packages/liedgut/pages/_document.js
@@ -36,6 +36,10 @@ export default class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
+          <script async defer src="https://sa.bdp-rps.app/latest.js"></script>
+          <noscript>
+            <img src="https://sa.bdp-rps.app/noscript.gif" alt="" />
+          </noscript>
         </body>
       </html>
     )
