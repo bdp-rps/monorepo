fc9883bd6a88635fbbf8b6c9ca7a730ed82c1c8e
Robin Weser <contact@weser.io>
2020-10-18 14:42:11 +0200

    use serverless for website

--- packages/website/next.config.js
+++ packages/website/next.config.js
@@ -13,7 +13,7 @@ const config = {
   experimental: {
     granularChunks: true,
   },
-  serverless: true,
+  target: 'serverless',
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
   webpack(config) {
     for (const rule of config.module.rules) {
