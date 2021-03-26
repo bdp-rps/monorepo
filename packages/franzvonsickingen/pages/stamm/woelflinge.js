03c0923aa9462a386e3a7dd788292a60f335fa5d
Timon Flick <s16c43@umwelt-campus.de>
2021-03-26 13:51:37 +0100

    found mistake, thanks michelle

--- packages/franzvonsickingen/pages/stamm/woelflinge.js
+++ packages/franzvonsickingen/pages/stamm/woelflinge.js
@@ -33,7 +33,7 @@ export default () => {
                         <Text>  Wölflinge sind die Jungen und Mädchen im Alter von sieben bis elf Jahren. Zusammen bilden wir eine Gruppe von etwa 15 bis 30 Kindern, die wir die „Meute” nennen. Wir treffen uns einmal in der Woche zur Meutenstunde und unternehmen gemeinsam lustige und spannende Dinge.
                         Die Meutenstunden finden in der Regel jeden Freitag von 16.45 Uhr bis 18.15 Uhr an der Pfadfinderhütte in Steinborn statt (Ausnahmen sind z.B. in den Schulferien, an Brücken- und Feiertagen).
                         Interessierte Kinder können gerne jederzeit ohne Anmeldung reinschauen. Bitte vergesst nicht euch Kleidung anzuziehen, die dreckig werden kann und warm hält (besonders im Frühjahr und Herbst). Außerdem sind feste Schuhe wichtig.
-            Ansprechpartner für die Wölflingsmeute: {representatives.map((representative) => representative.name)}
+            Ansprechpartner für die Wölflingsmeute: {representatives.map((representative) => representative.name + " ")}
                             <br />
             Was machen wir bei einer Meutenstunde? <br />
             Alles, was wir tun, soll uns Spaß machen. Deshalb machen wir ganz unterschiedliche Sachen. Wir spielen und basteln viel, singen und musizieren, wir entdecken die Natur, erleben Abenteuer und finden dabei viele gute Freunde. Bei jedem Treffen wollen wir aber auch etwas lernen. So verkleiden wir uns manchmal und gehen als Indianer auf die Pirsch, segeln als Piraten auf den Weltmeeren oder unternehmen eine Dschungelexpedition. Dabei lernen wir viel über andere Länder und lassen unserer Fantasie freien Lauf.
