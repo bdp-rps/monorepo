84d16e4353f867b6baede62513f978cd1b78f1e6
timonflick <contact@timonflick.com>
2021-05-11 19:22:52 +0200

    started adding watotokabisa

---
+++ packages/watotokabisa/pages/index.js
@@ -0,0 +1,63 @@
+import {
+  Box,
+  NavBar,
+  NavBarItem,
+  Text,
+  useTheme,
+  Spacer,
+  Tile,
+  Link,
+  Button,
+  ScrollView,
+} from '@bdp-rps/ui'
+import NextLink from 'next/link'
+
+import Layout from '../components/Layout'
+import Template from '../components/Template'
+
+const TextBox = ({ children }) => {
+  const theme = useTheme()
+
+  return (
+    <Box
+      paddingTop={0.5}
+      paddingBottom={0.5}
+      paddingLeft={2}
+      paddingRight={2}
+      alignSelf="flex-start"
+      extend={{ backgroundColor: theme.tokens.secondary }}>
+      <Text intent="category" color={theme.colors.primary}>
+        {children}
+      </Text>
+    </Box>
+  )
+}
+
+export default () => {
+  const theme = useTheme()
+
+  return (
+    <Template>
+      <Layout paddingTop={5} paddingBottom={5}>
+        <Box space={2}>
+          <Text intent="heading" color={theme.tokens.primary}>
+            Wer sind wir?
+          </Text>
+          <Text>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer et
+            ex et urna ullamcorper consequat nec sed nisi. Aliquam auctor sem
+            dignissim leo imperdiet, in interdum ante fermentum. Mauris et metus
+            ut lacus congue iaculis a a lacus. Curabitur fringilla malesuada
+            pretium. Quisque eget facilisis odio. Praesent tellus quam,
+            vestibulum ac sem non, faucibus convallis erat. Sed vel lorem
+            tincidunt, sodales tortor auctor, accumsan diam. Aliquam placerat
+            aliquet purus, quis volutpat enim tincidunt vel. Nunc nec maximus
+            libero. Nulla eget odio volutpat, dictum quam at, placerat sapien.
+            Duis suscipit eu nisl pretium scelerisque. Phasellus aliquam, mi
+            quis suscipit sodales, mi nisl rutrum lectus.
+          </Text>
+        </Box>
+      </Layout>
+    </Template>
+  )
+}
