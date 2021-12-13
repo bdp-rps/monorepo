55018e334a26edebd7130b9223cd6ccd41b344d7
Robin Weser <contact@weser.io>
2021-12-13 16:31:23 +0100

    fix franz setup

--- packages/franzvonsickingen/next.config.js
+++ packages/franzvonsickingen/next.config.js
@@ -12,19 +12,6 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 const config = {
   target: 'serverless',
   pageExtensions: ['js', 'jsx', 'md', 'mdx'],
-  webpack(config) {
-    for (const rule of config.module.rules) {
-      if (!rule.oneOf) {
-        continue
-      }
-
-      // removing the global css restriction
-      // do not touch this
-      delete rule.oneOf[5].issuer
-    }
-
-    return config
-  },
 }
 
 module.exports = withBundleAnalyzer(withMDX(config))
