d582165f26c1a070610f22bdb242e28e3e2bf7b6
Christian Gebel <christian.gebel@th-koeln.de>
2022-05-25 17:30:29 +0200

    Include search in map

--- packages/map/components/map/map.js
+++ packages/map/components/map/map.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useMemo, useState } from 'react'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
+import 'leaflet-control-geocoder'
 
 import getPlaces from '../../api/getPlaces'
 
@@ -9,6 +10,7 @@ import dynamic from 'next/dynamic'
 import { Box } from '@bdp-rps/ui'
 
 import PlaceMarker from './placeMarker'
+import LeafletControlGeocoder from '../../Leaflet-Control-Geocoder'
 
 const Map = ({ position, setPosition, placeMarkerVisible, filters = [] }) => {
   const [places, setPlaces] = useState([])
@@ -55,7 +57,10 @@ const Map = ({ position, setPosition, placeMarkerVisible, filters = [] }) => {
       {places?.map((place) => (
         <PlaceMarker {...place.attributes} />
       ))}
+    <LeafletControlGeocoder></LeafletControlGeocoder>
     </MapContainer>
+  
+    
   )
 }
 
