751bfe3d0f8211933ad1dd283c2051cf525effa9
Timon Flick <contact@timonflick.com>
2022-06-01 18:17:17 +0200

    added strapi

--- packages/website/pages/index.js
+++ packages/website/pages/index.js
@@ -9,40 +9,26 @@ import {
   Link,
   Button,
   ScrollView,
+  Grid,
 } from '@bdp-rps/ui'
 import NextLink from 'next/link'
 
 import Layout from '../components/Layout'
 import Template from '../components/Template'
 import PostTile from '../components/PostTile'
-import blogPosts from '../api/getBogposts'
+import blogPosts from '../api/getBlogposts'
 
 import getEvents from '../utils/getEvents'
 
 import manifest from '../public/blog-manifest.json'
-const [firstPost, ...otherPosts] = manifest
+import getBlogposts from '../api/getBlogposts'
 
-const TextBox = ({ children }) => {
+export default ({ events, posts }) => {
   const theme = useTheme()
 
-  return (
-    <Box
-      paddingTop={0.5}
-      paddingBottom={0.5}
-      paddingLeft={2}
-      paddingRight={2}
-      alignSelf="flex-start"
-      extend={{ backgroundColor: theme.tokens.secondary }}>
-      <Text variant="category" color={theme.tokens.primary}>
-        {children}
-      </Text>
-    </Box>
-  )
-}
-
-export default ({ events }) => {
-  const theme = useTheme()
-
+  const [firstPost, ...otherPosts] = posts.data
+  console.log('firstPost', firstPost)
+  console.log('otherPosts', otherPosts)
   return (
     <Template>
       <Layout paddingTop={5} paddingBottom={5}>
@@ -57,10 +43,8 @@ export default ({ events }) => {
         </Box>
       </Layout>
       <Layout
-        grow={1}
         paddingTop={5}
         paddingBottom={10}
-        alignSelf="stretch"
         extend={{ backgroundColor: 'rgb(235, 235, 235)' }}>
         <Text variant="subtitle">Das läuft bei uns.</Text>
         <Box paddingTop={2} direction={['column', , , 'row']} space={4}>
@@ -68,12 +52,11 @@ export default ({ events }) => {
             <PostTile highlight {...firstPost} />
           </Box>
           <Box grow={1} space={4}>
-            {otherPosts.splice(0, 2).map((post) => (
-              <PostTile key={post.id} {...post} />
+            {otherPosts.splice(0, 2).map((post, index) => (
+              <PostTile key={index} {...post} />
             ))}
           </Box>
         </Box>
-
         <Box paddingTop={9} alignSelf="flex-start" alignItems="flex-start">
           <Button href="/blog" size="large">
             Weitere Beiträge
@@ -86,12 +69,14 @@ export default ({ events }) => {
 
 export async function getStaticProps() {
   const events = await getEvents()
+  const posts = await getBlogposts()
 
   return {
     // alle 20 minuten
     revalidate: 1200,
     props: {
       events,
+      posts,
     },
   }
 }
