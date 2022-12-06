2fdb7db5edade5a867998886d1dac6176763f21e
Robin Weser <contact@weser.io>
2022-12-06 12:38:29 +0100

    fix form

--- packages/watotokabisa/pages/api/cardMail.js
+++ packages/watotokabisa/pages/api/cardMail.js
@@ -11,11 +11,66 @@ export default async function handler({ body }, res) {
     },
   })
 
+  const {
+    amountMeal,
+    amountGarden,
+    amountZiege,
+    porto,
+    name,
+    street,
+    postcode,
+    city,
+    email,
+  } = body
+
+  const moneyRequired = `
+Bitte überweise den offenen Betrag zeitnah auf unser Konto:
+Förderverein Watoto Kabisa e.V.
+DE12 5405 0220 0034 5389 91
+BIC: MALADE51KLS
+Stadtsparkasse Kaiserslautern
+
+Wir versenden die Karten, sobald wir das Geld erhalten haben!
+`
+
+  const text = `
+  Hallo ${name},
+  
+Vielen Dank für deine Kartenbestellung!
+
+Bevor wir die Karten verschicken können, brauchen wir eine kurze Bestätigung über die Richtigkeit der Bestellung.
+Antworte uns dazu einfach kurz auf diese E-Mail.
+
+${amountMeal ? `${amountMeal}x Schulspeisung: ${amountMeal * 10}€` : ''}
+${amountGarden ? `${amountGarden}x Schulspeisung: ${amountGarden * 20}€` : ''}
+${amountZiege ? `${amountZiege}x Schulspeisung: ${amountZiege * 30}€` : ''}
+inkl. Porto: ${porto ? 'Ja' : 'Nein'}
+--------------
+Gesamt: <b>${
+    amountMeal * 10 + amountGarden * 20 + amountZiege * 30 + porto ? 1.55 : 0
+  }€</b>
+
+
+${
+  withdrawMoney
+    ? 'Wir buchen den Betrag in den nächsten Tagen von deinem Konto ab.'
+    : moneyRequired
+}
+
+Lieferadresse:
+${body}
+${street}
+${postcode} ${city}
+
+Liebe Grüße,
+Der Vorstand
+Watoto Kabisa e.V.`
+
   const mail = {
-    from: `"Karten Service" <karten@watoto-kabisa.de>`,
-    to: 'karten@watoto-kabisa.de',
-    subject: 'Bestellung Karten - ' + body.name,
-    text: JSON.stringify(body, null, 2),
+    from: '"Karten Service" <karten@watoto-kabisa.de>',
+    to: `karten@watoto-kabisa.de, ${email}`,
+    subject: 'Bestellung Karten - ' + name,
+    text,
   }
 
   // send mail with defined transport object
