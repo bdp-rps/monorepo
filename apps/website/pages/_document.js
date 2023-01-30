eb0160b17c8e45d3d501b6d2ba36dccb735370be
Robin Weser <contact@weser.io>
2023-01-30 19:18:50 +0100

    remove icons from git

--- apps/website/pages/_document.js
+++ apps/website/pages/_document.js
@@ -24,10 +24,10 @@ export default class MyDocument extends Document {
     return (
       <Html lang="de">
         <Head>
-          <meta httpEquiv="content-type" content="text/html; charset=utf-8" />
+          <meta httpEquiv="content-type" content="text/html;charset=utf-8" />
           <meta
             name="viewport"
-            content="width=device-width,height=device-height,initial-scale=1, viewport-fit=cover"
+            content="width=device-width,height=device-height,initial-scale=1,viewport-fit=cover"
           />
         </Head>
         <body>
