7682b4b130eedd462b776d95d60b64ecca956ec4
Robin Weser <contact@weser.io>
2023-02-07 11:11:06 +0100

    fix liedgut app build

--- apps/liedgut/pages/[id]/index.js
+++ apps/liedgut/pages/[id]/index.js
@@ -92,7 +92,7 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const songData = require('@bdp-rps/liedgut/lib/songs/' + params.id + '.json')
+  const songData = require('@bdp-rps/liedgut/src/songs/' + params.id + '.json')
 
   return {
     props: {
