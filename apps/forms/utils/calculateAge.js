e51274d062cef08960d0318d1cab1ce1e0bdb9a0
christiangebel <dev.chrgebel@gmail.com>
2024-07-02 19:07:05 +0200

    feat: Kursanmeldung Prototype

---
+++ apps/forms/utils/calculateAge.js
@@ -0,0 +1,16 @@
+export default function calculateAge(birthday) {
+  const birthDate = new Date(birthday)
+  const today = new Date()
+  let age = today.getFullYear() - birthDate.getFullYear()
+  const monthDifference = today.getMonth() - birthDate.getMonth()
+
+  // If the birthday hasn't occurred yet this year, subtract one year from age.
+  if (
+    monthDifference < 0 ||
+    (monthDifference === 0 && today.getDate() < birthDate.getDate())
+  ) {
+    age--
+  }
+
+  return age
+}
