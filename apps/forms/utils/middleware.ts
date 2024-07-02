10d1ad0bae43109332f16b411ea2744366c82f3d
christiangebel <dev.chrgebel@gmail.com>
2024-07-02 20:12:15 +0200

    feat: push auth with matcher

---
+++ apps/forms/utils/middleware.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from 'next/server'
+
+export function middleware(req) {
+  const basicAuth = req.headers.get('authorization')
+  const url = req.nextUrl
+
+  if (basicAuth) {
+    const authValue = basicAuth.split(' ')[1]
+    const [user, pwd] = atob(authValue).split(':')
+
+    if (user === 'rrkurs' && pwd === 'kurs2024') {
+      return NextResponse.next()
+    }
+  }
+
+  url.pathname = '/api/auth'
+
+  return NextResponse.rewrite(url)
+}
+
+export const config = {
+  matcher: '/formulare/kursanmeldung',
+}
