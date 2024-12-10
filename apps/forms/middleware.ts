5bfbbcce5965be35b83138764bdd1a39fc3a1ccb
christiangebel <dev.chrgebel@gmail.com>
2024-12-10 21:05:51 +0100

    fix: adjust matcher for basic auth

--- apps/forms/middleware.ts
+++ apps/forms/middleware.ts
@@ -45,5 +45,11 @@ export function middleware(req) {
 }
 
 export const config = {
-  matcher: ['/formulare/kursanmeldung', '/formulare/segelanmeldung'],
+  matcher: [
+    '/formulare/kursanmeldung/1',
+    '/formulare/kursanmeldung/2',
+    '/formulare/kursanmeldung/3',
+    '/formulare/kursanmeldung/4',
+    '/formulare/segelanmeldung',
+  ],
 }
