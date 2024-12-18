2508d8f1ecca125143ca38105c3805efe5d2cb3b
christiangebel <dev.chrgebel@gmail.com>
2024-12-18 16:12:40 +0100

    fix: change to sendgrid

--- apps/forms/pages/api/send-notification-mail/index.js
+++ apps/forms/pages/api/send-notification-mail/index.js
@@ -1,16 +1,8 @@
 require('dotenv').config()
-import nodemailer from 'nodemailer'
+import sgMail from '@sendgrid/mail'
 
-// Create a Nodemailer transporter outside the handler
-const transporter = nodemailer.createTransport({
-  host: process.env.MAIL_HOST,
-  port: process.env.MAIL_PORT,
-  secure: true,
-  auth: {
-    user: process.env.MAIL_USER,
-    pass: process.env.MAIL_PASS,
-  },
-})
+// Set the SendGrid API Key
+sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
@@ -20,32 +12,44 @@ export default async function handler(req, res) {
   try {
     const { entry, model } = req.body
     const {
-      userName,
-      mail,
-      courseIdentifier,
+      name = 'Unbekannt',
+      mail = '',
       courseFee = '100 Euro',
     } = entry || {}
 
+    const courseName = req.body.entry?.courseIdentifier || ''
+
     if (model === 'kursanmeldung') {
-      const info = await transporter.sendMail({
-        from: 'no-reply@bdp-rps.de',
+      const msg = {
         to: mail,
+        from: 'no-reply@bdp-rps.de',
         subject: 'Bestätigung deiner Kursanmeldung',
         html: `
-          <p>Hallo ${userName},</p>
-          <p>Hier kommt deine Anmeldebestätigung für den <strong>${courseIdentifier.toUpperCase()}</strong>.</p>
-          <p>Bitte überweise den Kursbeitrag von ${courseFee} bis zum <strong>26.02.2025</strong> auf das LV Konto:</p>
-          <p>
-              <strong>BdP LV RPS</strong> <br>
-              IBAN: DE18 5405 0220 0108 8104 25 <br>
-              BIC: MALADE51KLK
-          </p>
-          <p>...</p>
-        `,
-      })
+        <p>Hallo ${name},</p>
+            <p>hier kommt deine Anmeldebestätigung für den <strong>${courseName.toUpperCase()}</strong>.</p>
+            <p>Bitte überweise den Kursbeitrag von ${courseFee} bis zum <strong>26.02.2025</strong> auf das LV Konto:</p>
+            <p>
+                <strong>BdP LV RPS</strong> <br>
+                IBAN: DE18 5405 0220 0108 8104 25 <br>
+                BIC: MALADE51KLK
+            </p>
+            <p>
+                Gib dabei bitte als Verwendungszweck den Kurstitel und deinen Namen an, 
+                dann können wir das Geld leichter zuordnen.
+            </p>
+            <p>
+                Bald wird sich die jeweilige Kursleitung bei dir melden. Falls du vorher noch Fragen hast, 
+                kannst du uns gerne eine Mail an <a href="mailto:ausbildung@bdp-rps.de">ausbildung@bdp-rps.de</a> schicken.
+            </p>
+            <p>Wir freuen uns, dass du bei der nächsten Kurssaison dabei bist!</p>
+            <p>Liebe Grüße und gut Pfad,</p>
+            <p>Deine Landesbeauftragten für Ausbildung, Anna und Lilli</p>`,
+      }
 
-      console.log('Email sent:', info.messageId)
-      return res.status(200).json({ success: `Email sent: ${info.messageId}` })
+      const response = await sgMail.send(msg)
+
+      console.log('Email sent:', response)
+      return res.status(200).json({ success: 'Email sent successfully' })
     } else {
       return res.status(400).json({ error: 'Invalid model, email not sent' })
     }
