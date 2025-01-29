86b2b995fafba43d5789f9f7ed2c68bce0647e87
Robin Weser <contact@weser.io>
2025-01-29 23:07:11 +0100

    add sonderurlaub form

--- apps/forms/pages/formulare/sonderurlaub.js
+++ apps/forms/pages/formulare/sonderurlaub.js
@@ -24,58 +24,8 @@ import rates from '../../utils/rates'
 import Wrapper from '../../templates/Wrapper'
 import Reisekosten from '../../templates/Reisekosten'
 
-function CarForm({ onSubmit }) {
-  const kilometer = useBaseField({
-    name: 'kilometer',
-    required: true,
-    validation: {
-      'Bitte nur Zahlen eingeben': (value) => value.match(/^\d+$/) !== null,
-    },
-  })
-  const count = useBaseField({
-    name: 'personen',
-    required: true,
-    value: '1',
-  })
-
-  const { submit, reset } = useForm(kilometer, count)
-
-  return (
-    <Box space={3}>
-      <TextInput label="Kilometer" {...kilometer.props} />
-      <SelectInput label="Personen" {...count.props}>
-        <option value="1">1</option>
-        <option value="2">2</option>
-        <option value="3">3</option>
-        <option value="4">4</option>
-        <option value="5">5</option>
-        <option value="6+">6+</option>
-      </SelectInput>
-      <Box>
-        <Button
-          onClick={(e) => {
-            e.preventDefault()
-
-            submit((isValid, data) => {
-              if (isValid) {
-                onSubmit({
-                  ...data,
-                  rate: rates[data.personen],
-                })
-                reset()
-              }
-            })
-          }}>
-          Hinzufügen
-        </Button>
-      </Box>
-    </Box>
-  )
-}
-
 export default function Page({ defaultData, defaultGenerated }) {
   const router = useRouter()
-  const [routes, setRoutes] = useState(defaultData.routes || [])
   const [modalVisible, setModalVisible] = useScrollBlockingOverlay(false)
   const [error, setError] = useState(false)
   const [generated, setGenerated] = useState(defaultGenerated)
@@ -90,12 +40,20 @@ export default function Page({ defaultData, defaultGenerated }) {
     name: 'name',
     required: true,
   })
+  const gender = useField({
+    name: 'gender',
+    required: true,
+  })
+  const group = useField({
+    name: 'group',
+    required: true,
+  })
   const event = useField({
     name: 'event',
     required: true,
   })
-  const location = useField({
-    name: 'location',
+  const boss = useField({
+    name: 'boss',
     required: true,
   })
   const startDate = useField({
@@ -106,62 +64,33 @@ export default function Page({ defaultData, defaultGenerated }) {
     name: 'endDate',
     required: true,
   })
-  const destination = useField({
-    name: 'destination',
-    required: true,
-  })
-  const note = useField({
-    name: 'note',
-  })
-  const iban = useField({
-    name: 'iban',
-  })
-  const place = useField({ name: 'place', required: true })
-  const date = useField({ name: 'date', required: true })
 
   const { submit, reset } = useForm(
     name,
     event,
-    location,
+    group,
+    gender,
     startDate,
     endDate,
-    destination,
-    note,
-    iban,
-    place,
-    date
+    boss
   )
 
-  const year = new Date(startDate.value).getFullYear()
-
-  const totalPrice = routes.reduce(
-    (total, { kilometer, personen }) => total + kilometer * rates[personen],
-    0
-  )
-
-  const totalValue = Math.floor(totalPrice * 100) / 100
-
-  const fileName =
-    year + '__' + name.value + '_' + event.value + '_' + totalValue
+  const fileName = 'Sonderurlaub__' + event.value + '_' + name.value
 
   useEffect(() => {
     const query = {
       name: name.value,
       event: event.value,
-      location: location.value,
+      gender: gender.value,
+      boss: boss.value,
+      group: group.value,
       startDate: startDate.value,
       endDate: endDate.value,
-      destination: destination.value,
-      iban: iban.value,
-      place: place.value,
-      note: note.value,
-      date: date.value,
-      routes,
     }
 
     router.replace(
       {
-        pathname: '/formulare/reisekosten',
+        pathname: '/formulare/sonderurlaub',
         query: {
           data: btoa(JSON.stringify(query)),
         },
@@ -174,15 +103,11 @@ export default function Page({ defaultData, defaultGenerated }) {
   }, [
     name.value,
     event.value,
-    location.value,
+    group.value,
+    gender.value,
+    boss.value,
     startDate.value,
     endDate.value,
-    destination.value,
-    note.value,
-    iban.value,
-    place.value,
-    date.value,
-    routes,
   ])
 
   if (!isMounted) {
@@ -192,15 +117,16 @@ export default function Page({ defaultData, defaultGenerated }) {
   if (generated) {
     const data = [
       ['Name', name.value],
+      ['Stamm', group.value],
       ['Veranstaltung', event.value],
-      ['Ort', location.value],
-      ['Reiseweg', destination.value],
+      ['Start', startDate.value],
+      ['End', endDate.value],
     ]
 
     const body = [
-      'Hey Cätch,',
+      'Hey Robin,',
       '',
-      'Anbei meine Reisekostenabrechnung mit folgenden Daten:',
+      'Ich brauche einen Antrag für Sonderurlaub mit folgenden Daten:',
       '',
       ...data.map((pair) => pair.join(': ')),
       '',
@@ -215,44 +141,46 @@ export default function Page({ defaultData, defaultGenerated }) {
     return (
       <Template>
         <Layout paddingTop={10} paddingBottom={20} space={8} grow={1}>
-          <Text variant="title">Reisekostenabrechung - Auto</Text>
+          <Text variant="title">Antrag auf Sonderurlaub</Text>
           <Box space={4} alignItems="flex-start">
             <Box>
               <Button
-                href={`mailto:kasse@bdp-rps.de?subject=Fahrtkosten ${
+                href={`mailto:robin@bdp-rps.de?subject=Sonderurlaub ${
                   event.value
-                } ${year} - ${name.value}&body=${body.join('%0D%0A')}`}>
-                E-Mail erstellen
+                } – ${name.value}&body=${body.join('%0D%0A')}`}>
+                Beantragen
               </Button>
             </Box>
-            <Box
-              as={PDFDownloadLink}
-              grow={1}
-              extend={{ textDecoration: 'none' }}
-              document={
-                <Wrapper>
-                  <Document>
-                    <Reisekosten
-                      name={name.value}
-                      event={event.value}
-                      location={location.value}
-                      startDate={startDate.value}
-                      endDate={endDate.value}
-                      destination={destination.value}
-                      iban={iban.value}
-                      place={place.value}
-                      date={date.value}
-                      note={note.value}
-                      routes={routes}
-                    />
-                  </Document>
-                </Wrapper>
-              }
-              fileName={fileName + '.pdf'}>
-              {({ blob, url, loading, error }) => (
-                <Button loading={loading}>Als PDF herunterladen</Button>
-              )}
-            </Box>
+            {localStorage.getItem('show') && (
+              <Box
+                as={PDFDownloadLink}
+                grow={1}
+                extend={{ textDecoration: 'none' }}
+                document={
+                  <Wrapper>
+                    <Document>
+                      {/* <Reisekosten
+                        name={name.value}
+                        event={event.value}
+                        location={location.value}
+                        startDate={startDate.value}
+                        endDate={endDate.value}
+                        destination={destination.value}
+                        iban={iban.value}
+                        place={place.value}
+                        date={date.value}
+                        note={note.value}
+                        routes={routes}
+                      /> */}
+                    </Document>
+                  </Wrapper>
+                }
+                fileName={fileName + '.pdf'}>
+                {({ blob, url, loading, error }) => (
+                  <Button loading={loading}>Als PDF herunterladen</Button>
+                )}
+              </Box>
+            )}
             <Box>
               <Button onClick={() => setGenerated(false)}>Bearbeiten</Button>
             </Box>
@@ -264,22 +192,9 @@ export default function Page({ defaultData, defaultGenerated }) {
 
   return (
     <>
-      <Modal
-        title="Strecke hinzufügen"
-        visible={modalVisible}
-        zIndex={10}
-        onClose={() => setModalVisible(false)}>
-        <CarForm
-          onSubmit={(data) => {
-            setError(false)
-            setRoutes([...routes, data])
-            setModalVisible(false)
-          }}
-        />
-      </Modal>
       <Template>
         <Layout paddingTop={10} paddingBottom={20} space={8}>
-          <Text variant="title">Fahrtkosten - Auto</Text>
+          <Text variant="title">Antrag auf Sonderurlaub</Text>
           <Box
             as="form"
             noValidate
@@ -287,17 +202,12 @@ export default function Page({ defaultData, defaultGenerated }) {
             onReset={(e) => {
               e.preventDefault()
 
-              router.push('/formulare/reisekosten')
+              router.push('/formulare/sonderurlaub')
             }}
             onSubmit={(e) => {
               e.preventDefault()
 
               submit((isValid, data) => {
-                if (routes.length === 0) {
-                  setError(true)
-                  return
-                }
-
                 if (isValid) {
                   setGenerated(true)
                 }
@@ -308,76 +218,34 @@ export default function Page({ defaultData, defaultGenerated }) {
               placeholder="Vor- und Nachname"
               {...name.props}
             />
+            <SelectInput label="Geschlecht" {...gender.props}>
+              <option value=""></option>
+              <option value="female">Weiblich</option>
+              <option value="male">Männlich</option>
+              <option value="divers">Divers</option>
+            </SelectInput>
+            <TextInput label="Stamm" {...group.props} />
             <TextInput
               label="Veranstaltung"
-              placeholder="z.B. Herbst-SST 2023"
+              placeholder="z.B. Osterkurse"
               {...event.props}
             />
-            <TextInput label="Veranstaltungsort" {...location.props} />
             <TextInput label="Start-Datum" type="date" {...startDate.props} />
             <TextInput label="End-Datum" type="date" {...endDate.props} />
-            <TextInput label="Reiseweg" {...destination.props} />
             <TextArea
-              label="Kommentar"
-              placeholder="z.B. inkl. Materialtransport, daher so viel"
-              {...note.props}
-            />
-            <span />
-            <Box space={1}>
-              <Box space={6}>
-                <Box alignSelf={['stretch', , 'flex-start']}>
-                  <Button onClick={() => setModalVisible(true)}>
-                    Strecke hinzufügen
-                  </Button>
-                </Box>
-                {routes.length > 0 && (
-                  <Box space={2}>
-                    {routes.map((route, index) => (
-                      <Box
-                        direction="row"
-                        justifyContent="space-between"
-                        space={4}
-                        alignItems="center">
-                        <Box>
-                          <Text>
-                            {route.kilometer}km ({route.personen}P) ={' '}
-                            {toEuro(route.kilometer * rates[route.personen])}
-                          </Text>
-                        </Box>
-                        <Box>
-                          <Button
-                            size="small"
-                            variant="secondary"
-                            intent="negative"
-                            onClick={() =>
-                              setRoutes(routes.filter((_, i) => i !== index))
-                            }>
-                            Löschen
-                          </Button>
-                        </Box>
-                      </Box>
-                    ))}
-                  </Box>
-                )}
-              </Box>
-              {error && (
-                <Text variant="note" color="foreground.destructive">
-                  Füge mindestens eine Strecke hinzu.
-                </Text>
-              )}
-            </Box>
-            <span />
-
-            <TextInput
-              label="IBAN"
-              description="Falls bereits bekannt, einfach leer lassen!"
-              {...iban.props}
+              label="Anschrift der Ansprechperson"
+              placeholder={`z. Hd. Max Mustermann
+Muster GmbH
+Musterstraße 1
+12345 Musterstadt`}
+              description="Für den Briefkopf – Die Anschrift der Person, die letztendlich diesen Antrag bekommt"
+              type="date"
+              {...boss.props}
+              extend={{
+                '> textarea': { resize: 'none', minHeight: '100px !important' },
+              }}
             />
 
-            <Box direction={['column', , 'row']} space={4} alignItems="stretch">
-              <TextInput label="Ort" {...place.props} />
-              <TextInput label="Datum" type="date" {...date.props} />
-            </Box>
             <span />
 
             <Box
