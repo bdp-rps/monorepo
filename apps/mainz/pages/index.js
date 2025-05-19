902e36ffc0af1a1b986337c7850be3440079bee4
TimonFlick <50593176+TimonFlick@users.noreply.github.com>
2025-05-19 13:26:00 +0200

    Mainz updates seo (#1616)
    * Menü copy updates
    
    * index image mobile change
    
    * work in progress
    
    * work in progress
    
    * work in progress

--- apps/mainz/pages/index.js
+++ apps/mainz/pages/index.js
@@ -10,6 +10,9 @@ import {
   Button,
   ScrollView,
   Grid,
+  IconCalendar,
+  IconClock,
+  IconCompass,
 } from '@bdp-rps/ui'
 import NextLink from 'next/link'
 import Head from 'next/head'
@@ -23,10 +26,127 @@ import getEvents from '../utils/getEvents'
 import { getBlogposts, getBlogpostsMainz } from '../api/getBlogposts'
 import { useState } from 'react'
 
+let gruppenDaten = [
+  {
+    img: 'schlange',
+    name: 'Meute Schlange',
+    age: 'von sieben bis elf',
+    place: 'Im Stammesheim',
+    time: 'ab 17:00 Uhr bis 18:30 Uhr',
+    day: 'Mittwochs',
+    description:
+      'Wir sind eine fröhliche und neugierige Gruppe von Kindern im Alter von 7 bis 11 Jahren, die gemeinsam die Welt der Pfadfinder entdecken. Bei uns steht das spielerische Lernen und die Entwicklung von Teamgeist im Vordergrund. Wir basteln, singen, spielen und erleben spannende Abenteuer in der Natur. Durch gemeinsame Aktivitäten und kleine Herausforderungen lernen wir, Verantwortung zu übernehmen und uns gegenseitig zu unterstützen. Unsere Gruppenstunden sind geprägt von Kreativität, Bewegung und viel Spaß.',
+    since: '2022',
+    groupLeaders: 'Anna und Mimi',
+  },
+  {
+    img: 'salix-alba',
+    name: 'Salix Alba',
+    age: 'von elf bis vierzehndreizehn',
+    place: 'Im Sommer im Garten und im Winter im Stammesheim',
+    time: 'ab 17:00 Uhr bis 18:30 Uhr',
+    day: 'Donnerstags',
+    description:
+      'Wir sind eine Gruppe von Jugendlichen, die ihre Begeisterung für die Natur, Gemeinschaft und Musik teilt. Bei uns steht das gemeinsame Erleben im Vordergrund - ob beim Wandern, Zelten oder Musizieren. Unsere Fahrt nach Slowenien war dabei nur einer von vielen Höhepunkten. In unseren Gruppenstunden entdecken wir die Natur, lernen neue Lieder und wachsen als Gruppe zusammen.',
+    since: '2024',
+    groupLeaders: 'Thore und Timon',
+  },
+  {
+    img: 'ignis-hedera',
+    name: 'Ignis Hedera',
+    age: 'von zwölf bis fünfzehn',
+    place: 'Im Sommer im Garten und im Winter im Stammesheim',
+    time: 'ab 17:00 Uhr bis 18:30 Uhr',
+    day: 'Mittwochs',
+    description:
+      'Wir sind eine lebendige Gruppe von Jugendlichen, die gemeinsam die Pfadfinderwelt erkundet. Bei uns steht der Spaß an der Gemeinschaft und das Entdecken neuer Fähigkeiten im Mittelpunkt. Ob beim Kochen über dem Feuer, bei Geländespielen oder beim gemeinsamen Musizieren - wir lernen spielerisch, Verantwortung zu übernehmen und als Team zusammenzuarbeiten. Unsere Gruppenstunden sind geprägt von Kreativität, Abenteuer und gegenseitigem Respekt.',
+    since: '2023',
+    groupLeaders: '',
+  },
+  {
+    img: 'raueber',
+    name: 'RäubeR/Runde',
+    age: 'von achtzehn bis anfang dreißig',
+    place: 'Im Garten, im Stammesheim oder manchmal in der Kneipe',
+    time: 'ab 19:00 Uhr',
+    day: 'Mittwochs',
+    description:
+      'Wir sind eine engagierte Gruppe von jungen Erwachsenen im Alter von etwa 18 bis Mitte 30, die mit Herz und Leidenschaft das Pfadfinderleben aktiv gestalten. Gemeinsam organisieren wir alles, was die Pfadfindergruppe am Laufen hält – von den wöchentlichen Gruppenstunden über spannende Fahrten bis hin zu unvergesslichen Lagern.Unser Ziel ist es, den jüngeren Generationen spannende und lehrreiche Abenteuer zu ermöglichen, während wir gleichzeitig selbst als Team zusammenwachsen und Verantwortung übernehmen. Wir legen großen Wert auf Zusammenarbeit, Kreativität und Spaß, während wir die Grundlagen der Pfadfinderbewegung in die Praxis umsetzen.',
+    since: '2022',
+    groupLeaders: '',
+  },
+]
+
+const InfoPoint = ({ info, title }) => {
+  return (
+    info && (
+      <Box>
+        <Text variant="note">{title}</Text>
+        <Text>{info}</Text>
+      </Box>
+    )
+  )
+}
+
+const GroupComponent = ({ data }) => {
+  const { name, img, age, day, place, description, since, groupLeaders, time } =
+    data
+  return (
+    <Tile
+      title={<Text>{name}</Text>}
+      image={'/images/gruppen/' + img + '.jpg'}
+      imageHeight={400}>
+      <Box space={6}>
+        <Box space={2}>
+          <Box direction="row" space={2}>
+            <Box direction="row" space={1} alignItems="center">
+              <IconCalendar size={18} />
+              <Text variant="note">{day}</Text>
+            </Box>
+            <Box direction="row" space={1} alignItems="center">
+              <IconClock size={18} />
+              <Text variant="note">{time}</Text>
+            </Box>
+          </Box>
+          <Box direction="row" space={1} alignItems="center" flexWrap="wrap">
+            <Box>
+              <IconCompass size={18} />
+            </Box>
+            <Box>
+              <Text variant="note">{place}</Text>
+            </Box>
+          </Box>
+        </Box>
+        <Box
+          extend={{
+            borderTop: '1px solid',
+            borderColor: 'grey',
+          }}
+        />
+        <Box space={4}>
+          <Grid columns={['1fr', , '1fr 1fr']} gap={4}>
+            <InfoPoint
+              title="Unsere Gruppenleitungen sind"
+              info={groupLeaders}
+            />
+            <InfoPoint title="Unsere Altersspanne ist" info={age} />
+            <InfoPoint title="Uns gibt es seit" info={since} />
+          </Grid>
+          <InfoPoint title="Wer wir sind" info={description} />
+        </Box>
+        <Box alignSelf="flex-start">
+          <Button href="mailto:pfadfinden@bdp-mainz.de" size="small">
+            Kontaktiere uns
+          </Button>
+        </Box>
+      </Box>
+    </Tile>
+  )
+}
+
 export default function page({ events, posts, postsMainz }) {
   const theme = useTheme()
 
-  const [isHover, setIsHover] = useState(false)
   const [firstPost, ...otherPosts] = posts
   const [firstPostsMainz, ...otherPostsMainz] = postsMainz
 
@@ -40,44 +160,33 @@ export default function page({ events, posts, postsMainz }) {
         />
       </Head>
       <Layout paddingTop={5} paddingBottom={5}>
-        <Box space={2}>
-          <Text variant="category">Ahoi und Hallo!</Text>
-          <Text>
-            Wir sind eine bunte Gruppe aus begeisterten BdP Pfadfinder*innen,
-            die schon lange von einem Stamm in Mainz träumen. Pfadfinden
-            bedeutet für uns Gemeinschaft, Freundschaften fürs Leben, Abenteuer
-            und einen sicheren Ort, an dem wir neue Ideen ausprobieren und ganz
-            wir selbst sein können.
-            <br />
-            Das wollen wir nicht nur für uns zurück in den Alltag holen, sondern
-            vor allem vielen Kindern und Jugendlichen hier in der Stadt
-            ermöglichen. Diese Idee soll nun endlich in die Tat umgesetzt
-            werden: <br />
-          </Text>
-          <Text>
-            Also komm in unsere Gruppenstunde! Immer{' '}
-            <Text subStyle="emphasis">Mittwochs</Text> außerhalb der Schulferien
-            von <Text subStyle="emphasis">17:00 bis 18:30 Uhr</Text> an unserem
-            Gruppenraum in der Liebfrauenkirche{' '}
-            <Text subStyle="emphasis">Moselstraße 30</Text>!
-          </Text>
-        </Box>
-        <Spacer size={8} />
-        <Tile
-          title="Die Menschen hinter der Stamm Tilia"
-          image={`/images/tilia_2023.jpeg`}
-          imageHeight={600}>
-          <Box>
+        <Box space={8}>
+          <Box space={2}>
+            <Text variant="category">Ahoi und Hallo!</Text>
             <Text>
-              Wir sind die Menschen hinter der Stamm Tilia Mainz.
-              Kontaktier uns doch gerne über{' '}
-              <Link href="mailTo:pfadfinden@bdp-mainz.de">
-                pfadfinden[at]bdp-mainz.de
-              </Link>
+              Wir sind eine bunte Gruppe aus begeisterten BdP Pfadfinder*innen,
+              die schon lange von einem Stamm in Mainz träumen. Pfadfinden
+              bedeutet für uns Gemeinschaft, Freundschaften fürs Leben,
+              Abenteuer und einen sicheren Ort, an dem wir neue Ideen
+              ausprobieren und ganz wir selbst sein können.
+              <br />
+              Das wollen wir nicht nur für uns zurück in den Alltag holen,
+              sondern vor allem vielen Kindern und Jugendlichen hier in der
+              Stadt ermöglichen. Diese Idee soll nun endlich in die Tat
+              umgesetzt werden: <br />
             </Text>
           </Box>
-        </Tile>
+          <Box>
+            <Text variant="subtitle">Das sind unsere Gruppen:</Text>
+            <Grid gap={5} columns={['1fr', , '1fr 1fr']}>
+              {gruppenDaten.map((data) => (
+                <GroupComponent data={data} key={data.name} />
+              ))}
+            </Grid>
+          </Box>
+        </Box>
       </Layout>
+
       <Layout
         grow={1}
         alignSelf="stretch"
