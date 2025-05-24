455ead54403dd9f7bcf23da567d536942bcec7d5
Robin Weser <contact@weser.io>
2025-05-24 15:51:27 +0200

    WIP rps weltreise

---
+++ apps/website/components/MapView.js
@@ -0,0 +1,26 @@
+import { MapContainer, TileLayer, Circle } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+import L from 'leaflet'
+
+export default function MapView({ position, zoom = 7, radius = 10000 }) {
+  return (
+    <MapContainer
+      center={position}
+      zoom={zoom}
+      style={{ height: 300, width: '100%' }}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      />
+      <Circle
+        center={position}
+        radius={Math.max(radius, 10000)} // radius in meters
+        pathOptions={{
+          color: 'blue',
+          fillColor: 'lightblue',
+          fillOpacity: 0.5,
+        }}
+      />
+    </MapContainer>
+  )
+}
