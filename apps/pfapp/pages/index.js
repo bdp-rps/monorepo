f5656ad14d2a65da62c85783eaf8226dc9ae3c57
TimonFlick <50593176+TimonFlick@users.noreply.github.com>
2025-06-01 20:35:11 +0200

    Pfapp pagination (#1619)
    * fixed list item in activity list
    
    * added pagination
    
    * fixed footer

--- apps/pfapp/pages/index.js
+++ apps/pfapp/pages/index.js
@@ -19,14 +19,65 @@ import GroupType from '../utils/groupType'
 
 import { getActivities } from '../api/getActivities'
 
+const usePagination = (totalItems, itemsPerPage, totalPages) => {
+  const router = useRouter()
+  const currentPage = Number(router.query.page) || 1
+  const [page, setPage] = useState(currentPage)
+
+  const handlePageChange = (newPage) => {
+    if (newPage < 1 || newPage > totalPages) return
+
+    router.push(
+      {
+        pathname: router.pathname,
+        query: { ...router.query, page: newPage },
+      },
+      undefined,
+      { shallow: false }
+    )
+  }
+
+  return { page: currentPage, totalPages, handlePageChange }
+}
+
+function Pagination({ page, totalPages, handlePageChange }) {
+  return (
+    <Box direction="row" justifyContent="space-between" alignItems="center">
+      <Box>
+        <Button
+          onClick={() => handlePageChange(page - 1)}
+          disabled={page === 1}>
+          Zurück
+        </Button>
+      </Box>
+      <Text>
+        Seite {page} von {totalPages}
+      </Text>
+      <Box>
+        <Button
+          onClick={() => handlePageChange(page + 1)}
+          disabled={page === totalPages}>
+          Weiter
+        </Button>
+      </Box>
+    </Box>
+  )
+}
+
 function ActivityListItem({ id, title, description, groupType }) {
   return (
     <ListItem href={'/' + id}>
-      <Box direction="row" space={2} alignItems="center">
-        {GroupType.toIcon(groupType, 24)}
-        <Box>
+      <Box
+        direction="row"
+        space={2}
+        alignItems="center"
+        extend={{ overflow: 'hidden' }}>
+        <Box width={36} height={36} flex="none">
+          {GroupType.toIcon(groupType, 24)}
+        </Box>
+        <Box flex={1}>
           <Text color="blue">{title}</Text>
-          <Text>{description}</Text>
+          <Text variant="note">{description}</Text>
         </Box>
       </Box>
     </ListItem>
@@ -35,7 +86,7 @@ function ActivityListItem({ id, title, description, groupType }) {
 
 function ActivityList({ activities }) {
   return (
-    <Box minHeight="95vh" paddingTop={4} paddingBottom={15} space={2}>
+    <Box space={2}>
       <Box>
         {activities.map((activity) => (
           <ActivityListItem
@@ -50,21 +101,35 @@ function ActivityList({ activities }) {
   )
 }
 
-export default function Page({ activities }) {
+export default function Page({ activities, pagination }) {
+  const { page, totalPages, handlePageChange } = usePagination(
+    pagination.total,
+    pagination.pageSize,
+    pagination.pageCount
+  )
+
   return (
     <Template>
-      <Layout paddingTop={4}>
+      <Layout paddingVertical={4} space={4}>
         <ActivityList activities={activities} />
+        <Pagination
+          page={page}
+          totalPages={totalPages}
+          handlePageChange={handlePageChange}
+        />
       </Layout>
     </Template>
   )
 }
 
 export async function getServerSideProps({ query }) {
-  const activities = await getActivities()
+  const { page = 1 } = query
+  const activities = await getActivities(page)
+
   return {
     props: {
       activities: activities.data,
+      pagination: activities.meta.pagination,
     },
   }
 }
